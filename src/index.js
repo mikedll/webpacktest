98de1b1855a75ce8ffe67d03971487e57daa2000
Michael Rivera <soymrmike@gmail.com>
2018-05-07 15:12:48 -0700

    css-loader and file-loader demonstrated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import _ from 'lodash';
-
+import './style.css';
+import PrettyPicture from './pretty-area2.jpg';
 function component() {
+  var rootElement = document.createElement('div');
+  
   var element = document.createElement('div');
 
   // Lodash, now imported by this script.
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
-  return element;
+  
+  var element2 = document.createElement('div');
+  var image = new Image();
+  image.src = PrettyPicture;  
+  element2.appendChild(image);
+
+  rootElement.append(element);
+  rootElement.append(element2);
+  
+  return rootElement;
 }
 
 document.body.appendChild(component());
