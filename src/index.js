6436b318337ddfea2e9af2a3ec88e3544d5c1c02
Michael Rivera <soymrmike@gmail.com>
2018-05-07 18:50:47 -0700

    removed file-loader examples

--- src/index.js
+++ src/index.js
@@ -1,29 +1,13 @@
 import _ from 'lodash';
-import './style.css';
-import PrettyPicture from './pretty-area2.jpg';
-import Data from './data.xml';
 
 function component() {
-  var rootElement = document.createElement('div');
   
   var element = document.createElement('div');
 
   // Lodash, now imported by this script.
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-  
-  var element2 = document.createElement('div');
-  var image = new Image();
-  image.src = PrettyPicture;  
-  element2.appendChild(image);
-
-  rootElement.append(element);
-  rootElement.append(element2);
-
-  console.log(Data);
-  
-  return rootElement;
+  return element;
 }
 
 document.body.appendChild(component());
